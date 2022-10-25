f5a81369214aa289d68045f4687ba8095b6569c1
Azat Harutyunyan <azat.harutyunyan@eemi.com>
2022-10-25 13:58:02 +0200

    async await done, remaining antdesign for the image

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,50 +1,46 @@
-import { useEffect, useContext, useState } from "react";
-import dayjs from "dayjs";
-import { Link } from "react-router-dom";
-import { AnimalContext } from "../context/animalContext";
-import { Table } from "antd";
-import "antd/dist/antd.css";
-import styled from "styled-components";
-import { timeColumns, animalsColumns } from "../util/columns";
+import { useEffect, useContext, useState } from 'react'
+import dayjs from 'dayjs'
+import { Link } from 'react-router-dom'
+import { AnimalContext } from '../context/animalContext'
+import { Table } from 'antd'
+import 'antd/dist/antd.css'
+import { animalsColumns } from '../util/columns'
 
 const HomePage = () => {
-  const [date, setDate] = useState(dayjs().format("YYYY-MM-DD"));
-  const [time, setTime] = useState(dayjs().format("HH:mm:ss"));
-  const [day, setDay] = useState(dayjs().format("dddd"));
+  const [date, setDate] = useState(dayjs().format('YYYY-MM-DD'))
+  const [time, setTime] = useState(dayjs().format('HH:mm:ss'))
+  const [day, setDay] = useState(dayjs().format('dddd'))
 
-  const { animal } = useContext(AnimalContext);
+  const { animal } = useContext(AnimalContext)
 
   useEffect(() => {
     const interval = setInterval(() => {
-      console.log("Interval ran");
-      setDate(dayjs().format("YYYY-MM-DD"));
-      setTime(dayjs().format("HH:mm:ss"));
-      setDay(dayjs().format("dddd"));
-    }, 5000);
+      console.log('Interval ran')
+      setDate(dayjs().format('YYYY-MM-DD'))
+      setTime(dayjs().format('HH:mm:ss'))
+      setDay(dayjs().format('dddd'))
+    }, 5000)
 
     return () => {
-      console.log("Interval cleared");
-      clearInterval(interval);
-    };
-  }, []);
+      console.log('Interval cleared')
+      clearInterval(interval)
+    }
+  }, [])
 
   return (
     <>
-      <Link to={"/list"}>
+      <Link to={'/list'}>
         <br /> clique sur la liste !
       </Link>
 
       <div>
         <p>
-          {" "}
           <br /> Date : {date}
         </p>
         <p>
-          {" "}
           <br /> Heure : {time}
         </p>
         <p>
-          {" "}
           <br /> Jour : {day}
         </p>
       </div>
@@ -61,7 +57,7 @@ const HomePage = () => {
         </p>
       )}
     </>
-  );
-};
+  )
+}
 
-export default HomePage;
+export default HomePage
--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -1,60 +1,52 @@
-import { useState, useEffect, useContext } from "react";
-import { Table } from "antd";
-import "antd/dist/antd.css";
-import { AnimalType } from "../util/AnimalDataType";
-import styled from "styled-components";
-import { Link } from "react-router-dom";
-import { AnimalContext } from "../context/animalContext";
-import { animalsColumns } from "../util/columns";
-
-const Button = styled.button`
-  background: transparent;
-  border-radius: 3px;
-  border: 2px solid black;
-  color: black;
-`;
+import { useState, useEffect, useContext } from 'react'
+import { Table } from 'antd'
+import 'antd/dist/antd.css'
+import { AnimalType } from '../util/AnimalDataType'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+import { AnimalContext } from '../context/animalContext'
+import { animalsColumns } from '../util/columns'
 
 const ListPage = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [list, setList] = useState([]);
-  const { saveAnimal } = useContext(AnimalContext);
+  const [isLoading, setIsLoading] = useState(false)
+  const [list, setList] = useState([])
+  const { saveAnimal } = useContext(AnimalContext)
 
   const animalsColumnsWithSaveBtn = [
     ...animalsColumns,
     {
-      title: "Save",
-      key: "save",
+      title: 'Save',
+      key: 'save',
       render: (text: string, record: AnimalType) => (
         <Button
           onClick={() => {
-            console.log("record", record);
-            saveAnimal(record);
+            console.log('record', record)
+            saveAnimal(record)
           }}
         >
           Save
         </Button>
       ),
     },
-  ];
+  ]
 
-  const fetchAnimals = () => {
-    setIsLoading(true);
-    fetch("http://zoo-animal-api.herokuapp.com/animals/rand/10")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setList(data);
-      });
-    setIsLoading(false);
-  };
+  const fetchAnimals = async () => {
+    setIsLoading(true)
+    const response = await fetch(
+      'http://zoo-animal-api.herokuapp.com/animals/rand/10'
+    )
+    const data = await response.json()
+    setList(data)
+    setIsLoading(false)
+  }
 
   useEffect(() => {
-    fetchAnimals();
-  }, []);
+    fetchAnimals()
+  }, [])
 
   return (
     <div>
-      <Link to={"/"}>Go To HomePage</Link>
+      <Link to={'/'}>Go To HomePage</Link>
 
       <Table
         columns={animalsColumnsWithSaveBtn}
@@ -63,7 +55,14 @@ const ListPage = () => {
         pagination={false}
       />
     </div>
-  );
-};
+  )
+}
 
-export default ListPage;
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid black;
+  color: black;
+`
+
+export default ListPage
